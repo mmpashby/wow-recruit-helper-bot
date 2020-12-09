56f15662ddc43a322c6543112557d2c6e877694d
Pashbee <mark@pash.by>
2020-12-09 17:12:55 +0000

    fix: handle char not found better and also update dungeon count for shadowlands

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ from discord.ext import commands
 ## CONFIG RELATED CONSTS
 TOKEN = os.getenv('DISCORD_TOKEN')
 RAIDERIO_URL = "https://raider.io/api/v1"
-NO_DUNGEONS_CS = 11
-FIFTTEEN_TIMED = 161
+NO_DUNGEONS_CS = 8
+FIFTEEN_TIMED = 161
 
 
 # TODO: break out into multi-class for character return info etc
@@ -22,8 +22,8 @@ class RecruitDecision:
         self.role = role
         self.body = apirsp
         self.rolescore = self.get_role_score()
-        self.onehundredp = NO_DUNGEONS_CS * FIFTTEEN_TIMED
-        self.sixtysixp = round(((NO_DUNGEONS_CS * 2) / 3) * FIFTTEEN_TIMED)
+        self.ninetyninep = NO_DUNGEONS_CS * FIFTEEN_TIMED
+        self.sixtysixp = round(((NO_DUNGEONS_CS * 2) / 3) * FIFTEEN_TIMED)
         self.recruitanswer = self.getdecision()
 
     def __str__(self):
@@ -32,22 +32,23 @@ class RecruitDecision:
     def get_role_score(self):
         try:
             rolescore = self.body['mythic_plus_scores_by_season'][0]['scores'][self.role]
-        except KeyError:
+        except (KeyError, TypeError) as e:
             rolescore = 0
         return rolescore
 
     def getdecision(self):
         decision = "Unknown..."
-        if self.rolescore >= self.onehundredp:
+        if self.rolescore >= self.ninetyninep:
             decision = "Recruit! Very Good - 15+ on all dungeons likely."
         elif self.rolescore >= self.sixtysixp:
-            decision = "Check ilvl and gain more info - Average - in 66p for this season assuming 15+ 2/3 keys."
+            decision = "Recruit! Check ilvl and gain more info - Average - in 66p for this season assuming 15+ 2/3 keys."
         elif self.rolescore < self.sixtysixp:
             decision = "No - Below Average - below 66p for this season and likely needs work."
         return decision
 
     def readymsg(self):
         """Make message to discord look ok"""
+        print(self.body)
         prsp = (
             f"**CharName:** {self.body['name']}\n",
             f"**Class:** {self.body['class']}\n",
@@ -75,6 +76,7 @@ async def on_ready():
              help='Provides rudimentary recommendation engine for recruitment purposes.',
              usage='[PlayerName] [Role:dps/healer/tank] [Region] [Realm]')
 async def recruit(ctx, player: str, role: str, region: str, realm: str):
+    e = discord.Embed()
     sr = raiderio.resource("characters")
     s_params = {'player': player,
                 'role': role,
@@ -83,14 +85,17 @@ async def recruit(ctx, player: str, role: str, region: str, realm: str):
     stat_code, grsp =  sr.get_char(**s_params)
     if stat_code == 200:
         rsp = grsp
+        descision = RecruitDecision(role, rsp)
+        dmsg = ''.join(descision.readymsg())
+        e.set_image(url=rsp['thumbnail_url'])
+        await ctx.send(dmsg, embed=e)
     elif stat_code == 400:
         rsp = "Character Not Found..."
+        dmsg = ''.join(rsp)
+        await ctx.send(dmsg)
     else:
         rsp = "Unknown Error..."
-    descision = RecruitDecision(role, rsp)
-    dmsg = ''.join(descision.readymsg())
-    e = discord.Embed()
-    e.set_image(url=rsp['thumbnail_url'])
-    await ctx.send(dmsg, embed=e)
+        dmsg = ''.join(rsp)
+        await ctx.send(dmsg)
 
 bot.run(TOKEN)
